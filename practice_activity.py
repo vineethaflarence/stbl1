80e4723773b2a9f4181a98b5b2533b268d346667
vineethaflarence <75540202+vineethaflarence@users.noreply.github.com>
2022-08-03 15:21:28 +0530

    Add files via upload

---
+++ practice_activity.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("500x250")
+
+label_file_name = Label(root, text="Name")
+label_file_name.place(relx=0.3,rely=0.1,anchor= CENTER)
+
+input_file_name = Entry(root)
+input_file_name.place(relx=0.5,rely=0.1, anchor= CENTER)
+
+my_text= Text(root,height=5,width=40)
+my_text.place(relx=0.5,rely=0.4,anchor= CENTER)
+
+
+def clearInputFeild():
+    input_file_name.delete(0, END)
+    
+def clearTextarea():
+    my_text.delete(1.0, END)
+    
+def addData():
+    input_file_name.insert(END,"myfile")
+
+
+open_button=Button(root,text="Clear Input field", command=clearInputFeild)
+open_button.place(relx=0.25,rely=0.7,anchor=CENTER)
+save_button=Button(root, text="Clear Textarea", command=clearTextarea)
+save_button.place(relx=0.455,rely=0.7,anchor= CENTER)
+exit_button=Button(root, text="Add data to input feild", command=addData)
+exit_button.place(relx=0.7,rely=0.7,anchor= CENTER)
+
+root.mainloop()
+
